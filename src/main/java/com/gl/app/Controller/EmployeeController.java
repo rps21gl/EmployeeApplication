de7c65d9dc1c89d75fca9e939b9a92c5da7c9a38
Rahul Srivastava <rahul.p.srivastava@globallogic.com>
2024-06-27 11:24:39 +0530

    firstCommit

---
+++ src/main/java/com/gl/app/Controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.gl.app.Controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
